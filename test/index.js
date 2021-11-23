cf1cd967e813680d9e8533ddb4f3ad4bbe2334f8
Malte Hallström <malte.hallstrom@gmail.com>
2021-11-24 00:21:35 +0100

    test: add tests for classnames & tagnames

--- test/index.js
+++ test/index.js
@@ -9,17 +9,26 @@ import rehypeTabularCode from '../dist/index.js'
 const processHtml = (html) =>
   rehype().data('settings', { fragment: true }).use(rehypeTabularCode).processSync(html).toString()
 
-const processMarkdown = async (md) =>
+const processMarkdown = async (md, custom) =>
   (
-    await unified().use(remarkParse).use(remarkRehype).use(rehypeTabularCode).use(rehypeStringify).process(md)
+    await unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeTabularCode, custom)
+      .use(rehypeStringify)
+      .process(md)
   ).toString()
 
 const codeBlock = '<pre><code class="language-typescript"></code></pre>'
 const tabBlock =
-  '<div class="tabular-code"><ul class="tabular-code-tabs"><div class="code-tab active-tab"></div><div class="code-tab"></div></ul><div class="tabular-code-content"><div><pre><code class="language-ts">console.log(\'hello\')\n</code></pre></div><div class="hidden"><pre><code class="language-ts">console.log(\'hello\')\n</code></pre></div></div></div>\n'
+  '<div class="tabular-code"><ul class="tabular-code-tabs"><li class="tabular-code-tab active-tab">hello</li><li class="tabular-code-tab">hello2</li></ul><div class="tabular-code-content"><div><pre><code class="language-ts">console.log(\'hello\')\n</code></pre></div><div class="hidden"><pre><code class="language-ts">console.log(\'hello\')\n</code></pre></div></div></div>\n'
+const tabBlockCN =
+  '<div class="test"><ul class="tabular-code-tabs"><li class="tabular-code-tab active-tab">hello</li><li class="tabular-code-tab">hello2</li></ul><div class="tabular-code-content"><div><pre><code class="language-ts">console.log(\'hello\')\n</code></pre></div><div class="hidden"><pre><code class="language-ts">console.log(\'hello\')\n</code></pre></div></div></div>\n'
+const tabBlockTN =
+  '<main class="tabular-code"><ul class="tabular-code-tabs"><li class="tabular-code-tab active-tab">hello</li><li class="tabular-code-tab">hello2</li></ul><div class="tabular-code-content"><div><pre><code class="language-ts">console.log(\'hello\')\n</code></pre></div><div class="hidden"><pre><code class="language-ts">console.log(\'hello\')\n</code></pre></div></div></main>\n'
 
 test('rehype-tabular-code', async (t) => {
-  t.plan(3)
+  t.plan(5)
 
   const singleBlock = processHtml(codeBlock)
 
@@ -40,6 +49,28 @@ test('rehype-tabular-code', async (t) => {
   t.equal(
     tabBlockParsed,
     tabBlock,
-    'given markdown with two adjecent codeblocks, should combine them into one tabular block',
+    'given markdown with two adjecent codeblocks, should combine them into one tabular block with tab titles',
+  )
+
+  const tabBlockParsedCN = await processMarkdown(
+    "```ts tab='hello'\nconsole.log('hello')\n```\n```ts tab='hello2'\nconsole.log('hello')\n```",
+    { classnames: { container: 'test' } },
+  )
+
+  t.equal(
+    tabBlockParsedCN,
+    tabBlockCN,
+    'given a custom option for container classname, when parsing, should use custom classname',
+  )
+
+  const tabBlockParsedTN = await processMarkdown(
+    "```ts tab='hello'\nconsole.log('hello')\n```\n```ts tab='hello2'\nconsole.log('hello')\n```",
+    { tagnames: { container: 'main' } },
+  )
+
+  t.equal(
+    tabBlockParsedTN,
+    tabBlockTN,
+    'given a custom option for container tagname, when parsing, should use custom tagname',
   )
 })
